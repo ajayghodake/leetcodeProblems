e76b9f200f536b5bbe26ffebf93b2709474fcda5
Ajay Ghodake <110241243+ajayghodake@users.noreply.github.com>
2023-09-04 00:12:03 +0530

    Time: 4 ms (66.31%) | Memory: 43.6 MB (63.44%) - LeetSync

--- 1-two-sum/two-sum.java
+++ 1-two-sum/two-sum.java
@@ -25,7 +25,7 @@ class Solution {
 
 
 
-// with for loop logic
+// with for loop logic brute force
 //  for (int i = 0; i < num.length; i++) {
 //             for (int j = i + 1; j < num.length; j++) {
 //                 if (target == num[i] + num[j]) {
