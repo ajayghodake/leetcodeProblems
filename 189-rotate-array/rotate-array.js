89d58d694d30957e49bd315984e992a0d6cc606f
Ajay Ghodake <110241243+ajayghodake@users.noreply.github.com>
2024-03-03 17:55:13 +0530

    Time: 84 ms (45.84%) | Memory: 57.4 MB (39.79%) - LeetSync

---
+++ 189-rotate-array/rotate-array.js
@@ -0,0 +1,22 @@
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {void} Do not return anything, modify nums in-place instead.
+ */
+var rotate = function(nums, k) {
+    const reverseArr = (start, end) => {
+        while(start < end){
+            [nums[start], nums[end]] = [nums[end], nums[start]];
+            start++;
+            end--;
+        }
+    }
+    
+   k = k%nums.length;
+   reverseArr(0, nums.length-1);
+   reverseArr(0, k-1);
+   reverseArr(k, nums.length-1);
+
+
+return nums;
+};
