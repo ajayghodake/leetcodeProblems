6d6d4b4b8b2a238c45fc1898ddbdfc14d33a33ab
Ajay Ghodake <110241243+ajayghodake@users.noreply.github.com>
2024-03-03 18:03:51 +0530

    Time: 66 ms (97.34%) | Memory: 57.8 MB (36.26%) - LeetSync

--- 189-rotate-array/rotate-array.js
+++ 189-rotate-array/rotate-array.js
@@ -4,19 +4,24 @@
  * @return {void} Do not return anything, modify nums in-place instead.
  */
 var rotate = function(nums, k) {
-    const reverseArr = (start, end) => {
-        while(start < end){
-            [nums[start], nums[end]] = [nums[end], nums[start]];
-            start++;
-            end--;
-        }
-    }
+//     const reverseArr = (start, end) => {
+//         while(start < end){
+//             [nums[start], nums[end]] = [nums[end], nums[start]];
+//             start++;
+//             end--;
+//         }
+//     }
     
-   k = k%nums.length;
-   reverseArr(0, nums.length-1);
-   reverseArr(0, k-1);
-   reverseArr(k, nums.length-1);
+//    k = k%nums.length;
+//    reverseArr(0, nums.length-1);
+//    reverseArr(0, k-1);
+//    reverseArr(k, nums.length-1);
 
 
-return nums;
+// return nums;
+let n = nums.length;
+k %= n;
+
+let rotatedArr = nums.splice(n-k, k);
+nums.unshift(...rotatedArr);
 };
