6f4e073c4cff8ff19a26e8663a4b2e415e6a6cb6
Ajay Ghodake <110241243+ajayghodake@users.noreply.github.com>
2024-03-03 18:40:45 +0530

    Time: 81 ms (59.07%) | Memory: 57 MB (48.44%) - LeetSync

--- 189-rotate-array/rotate-array.js
+++ 189-rotate-array/rotate-array.js
@@ -5,29 +5,29 @@
  */
 var rotate = function(nums, k) {
 
-    // Approach 1
-    const reverseArr = (start, end) => {
-        while(start < end){
-            [nums[start], nums[end]] = [nums[end], nums[start]];
-            start++;
-            end--;
-        }
-    }
+//     // Approach 1
+//     const reverseArr = (start, end) => {
+//         while(start < end){
+//             [nums[start], nums[end]] = [nums[end], nums[start]];
+//             start++;
+//             end--;
+//         }
+//     }
     
-   k = k%nums.length;
-   reverseArr(0, nums.length-1);
-   reverseArr(0, k-1);
-   reverseArr(k, nums.length-1);
+//    k = k%nums.length;
+//    reverseArr(0, nums.length-1);
+//    reverseArr(0, k-1);
+//    reverseArr(k, nums.length-1);
 
 
-return nums;
+// return nums;
 
 
-// // Approach 2
-// let n = nums.length;
-// k %= n;
-// if(k>0){
-// let rotatedArr = nums.splice(n-k, k);
-// nums.unshift(...rotatedArr);
-// }
+// Approach 2
+let n = nums.length;
+k %= n;
+if(k>0){
+let rotatedArr = nums.splice(n-k, k);
+nums.unshift(...rotatedArr);
+}
 };
